26ca94e2176ddcaaf7a3fa1318ce4e538091ab74
Abhilash Velalam <avelalam@cohesity.com>
2019-02-24 14:02:17 -0800

    Minox fixes

--- viewbrowser/view_browser_exec.go
+++ viewbrowser/view_browser_exec.go
@@ -1,13 +1,15 @@
 // Copyright 2019 Cohesity Inc.
-//
+
 
 package main
 
 import (
+  "flag"
   "github.com/cohesity/cohesity-app-spec/viewbrowser/server"
 )
 
 func main() {
+  flag.Parse()
   rs := viewbrowserserver.NewFileBrowserServer()
   rs.Start()
 }
